7b0850fa4c91a3acb3627f2c284a5ea5bf3c582c
JordanTDT <106220229+JordanTDT@users.noreply.github.com>
2023-05-03 17:58:16 +0000

    =

---
+++ output/source/AutoProcessor/Sentence/FrameNet/SentenceAutoFramePredicate.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SentenceAutoFramePredicate = void 0;
+class SentenceAutoFramePredicate {
+}
+exports.SentenceAutoFramePredicate = SentenceAutoFramePredicate;
+//# sourceMappingURL=SentenceAutoFramePredicate.js.map
